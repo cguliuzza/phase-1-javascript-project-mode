8b9c0d5183ce642380883e475318a4cde1838e22
jaenwawe <nwawe.jae@gmail.com>
2021-08-27 13:08:25 -0500

    TJ directions ingredients

--- src/index.js
+++ src/index.js
@@ -1,25 +1,125 @@
+const dropDownElement = document.querySelector('.recipeType');  //uses ropDownElement.addEventListener to filter meals based on 'change' event
+//const dataForJSON = 'http://localhost:3000/recipes' //add url, db.json file location, server address, etc.
+const dataForJSON = null; //add url, db.json file location, server address, etc.
+const starImg = document.createElement('img');
+const recipeBlock = document.querySelector('#recipe-block');
+const oneRecipeBlock = document.querySelector('#one-recipe-block');
+const brNewLine = document.createElement('br');
+opensMeals();
+
+
+function opensMeals() {
+
 fetch('http://localhost:3000/recipes')
 .then(resp => resp.json())
 .then(allRecipesArray => {
-    const recipeBlock = document.querySelector('#recipe-block')
+    
     
     allRecipesArray.forEach((recipeObject) => {
-        console.log(recipeObject)
+     //console.log(recipeObject)
+
+       const recipeTitleP= document.createElement('p');
+       recipeTitleP.innerText = recipeObject.title;
+  
+       const starImg = document.createElement('img');
+       starImg.src= "css/pinkStar.jpg";
+       starImg.classList= ["favoriteStar"]
+
+
+        const oneTitleDiv=document.createElement('div');
+        oneTitleDiv.classList ="nextLine";
         
-        const recipeTitles = document.querySelector('#title')
-        recipeTitles.textContent = recipeObject.title
-        recipeBlock.append(recipeTitles)
+        
+        recipeBlock.append(oneTitleDiv);
+        const titleDiv= document.querySelector('.margin-top')
+       
 
-        const recipeIngredients = document.querySelector('#ingredients')
-        recipeIngredients.textContent = recipeObject.ingredients
-        recipeBlock.append(recipeIngredients)
+        oneTitleDiv.append(brNewLine,starImg, recipeTitleP);
+        recipeBlock.append(oneTitleDiv)
+        console.log(oneTitleDiv)
+        
+      
 
-        const recipeDirections = document.querySelector('#directions')
-        recipeDirections.textContent = recipeObject.directions
-        recipeBlock.append(recipeDirections)
+//Lower Half Ingredients and Directions
+//Find Object.ingredients  b/c its null now
+//center title
+      
 
-        const recipeTagNames = document.querySelector('#tags')
-        // recipeBlock.append(recipeTagNames)
+        starImg.addEventListener('click',(e) =>{
+            //console.log(e.target.parentElement.querySelector('p'));
+            const selectedDivTitle =e.target.parentElement.querySelector('p');
+            const selectedTitle = selectedDivTitle.innerText;
+            const selectedTitleDiv=document.createElement('div');
+            console.log(selectedDivTitle)
+            selectedDivTitle.classList =["nextLine", "middle"];
+
+            const unOrderedList = document.createElement('ul');
+            const orderedList = document.createElement('li');
+            selectedDivTitle.innerText= recipeTitleP;
+            console.log(selectedTitle);
+
+
+            starImg.src= "css/blueStar.jpg"
+   
+   
+          recipeBlock.remove();
+
+            recipeTitleP.innerText = selectedTitle;
+            console.log(recipeTitleP)
+           // recieptStrongTitleP = document.querySelector('#title');
+        //recieptStrongTitleP.innerText = selectedTitle;
+
+            const ingredientsLeft = document.querySelector("#id-ingredients");
+            ingredientsLeft.textContent = recipeObject["ingredients"];
+            
+    
+           //<p id="ingredients"><span><strong>Ingredients: </strong></span>
+             const directionsTitleH2 = document.createElement('h2');
+             directionsTitleH2.classList['bold'];
+             directionsTitleH2.innerText ='Title';
+
+             oneRecipeBlock.append(directionsTitleH2,selectedTitle);
+          
+            const directionsRight = document.querySelector("#id-directions");
+            directionsRight.textContent = recipeObject["directions"];
+         
+        
+            const recipeIngredients = document.createElement('ul');
+
+            console.log(recipeObject.ingredients)
+            recipeObject.ingredients.forEach((ingredient)=> {
+console.log(recipeIngredients);
+            const ingredientLi =document.createElement('li');
+            ingredientLi.innerText= ingredient;
+            recipeIngredients.append(ingredientLi);    
+
+            })
+
+            oneRecipeBlock.append(brNewLine,directionsRight,recipeIngredients);
+
+
+
+            //recipeIngredients.textContent = recipeObject.ingredients
+
+
+            oneRecipeBlock.append(recipeIngredients)
+           
+    
+            //oneRecipeBlock.append(recipeDirections)    
+            //const recipeTagNames = document.querySelector('#id-directions')
+            // recipeBlock.append(recipeTagNames)
+
+        })
+      // recipeBlock.append(recipeTitles)
     })
-
 })
+}
+
+
+
+
+
+/*
+const recipeTitles = document.querySelector('#title')
+recipeTitles.textContent = recipeObject.title
+*/
